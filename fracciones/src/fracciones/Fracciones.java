b4ffea73080f64f404ded2972741598d75c48841
JacobHawk603 <62320033+JacobHawk603@users.noreply.github.com>
2021-10-15 20:21:08 -0500

    practica de las fracciones

--- fracciones/src/fracciones/Fracciones.java
+++ fracciones/src/fracciones/Fracciones.java
@@ -16,6 +16,16 @@ public class Fracciones {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        fraccion F1= new fraccion(1,3);
+        fraccion F2 = new fraccion(1,2);
+        fraccion F= new fraccion(1,1);  //la inicializo con 1,1 para evitar errores matemáticos de indeterminaciones
+        
+        F=F1.sumar(F2);
+        
+        System.out.println("la fracción 1 es: "+F1.toString() +"\n"
+        +"la fraccion 2 es: "+F2.toString());
+        
+        System.out.println(F1.toString() + " + " + F2.toString() +" = " +F.toString());
     }
     
 }
--- fracciones/src/fracciones/fraccion.java
+++ fracciones/src/fracciones/fraccion.java
@@ -10,5 +10,56 @@ package fracciones;
  * @author Jacob
  */
 public class fraccion {
+    int numerador, denominador;
+
+    public fraccion(int numerador, int deniminador) {
+        this.numerador = numerador;
+        this.denominador = deniminador;
+    }
     
+    public fraccion sumar(fraccion frac2){
+        fraccion resultado= new fraccion(1,1);
+        
+        resultado.denominador= denominador*frac2.denominador;
+        resultado.numerador=(numerador*frac2.denominador)+(frac2.numerador*denominador);
+        resultado.simplificar();
+        
+        return resultado;
+    }
+    
+    private void simplificar(){
+        int menor=1, mayor=1, mcd=1;
+        
+        if (denominador==1 && numerador==1) {
+            return;
+        }else if (denominador==numerador) {
+            numerador=denominador=1;
+            return;
+        }else if (numerador>denominador) {
+            mayor=numerador;
+            menor=denominador;
+        }else if (denominador>numerador) {
+            mayor=denominador;
+            menor=numerador;
+        }
+        
+        if (mayor%menor==0) {
+            numerador/=menor;
+            denominador/=menor;
+            
+            return;
+        }
+        
+        for (int i = 1; i < menor; i++) {
+            if (numerador%i==0 &&denominador%i==0)
+                mcd=i;
+        }
+            numerador/=mcd;
+            denominador/=mcd;
+    }
+    
+    @Override
+    public String toString(){
+        return numerador + "/" + denominador;
+    }
 }
